9bd7993ce736ca23cbb4f5eb213ec8544e5a1ae8
lipovolt <498307481@qq.com>
2016-04-18 16:37:36 +0800

    1

--- App/Lib/Action/Storage/StorageAction.class.php
+++ App/Lib/Action/Storage/StorageAction.class.php
@@ -35,6 +35,80 @@ class StorageAction extends CommonAction{
         }
         $this->display();
 	}
+
+  /**
+      * 系统邮件发送函数
+      * @param string $to    接收邮件者邮箱
+      * @param string $name  接收邮件者名称
+      * @param string $subject 邮件主题 
+      * @param string $body    邮件内容
+      * @param string $attachment 附件列表
+      * @return boolean 
+      */
+    private function sendInventoryMail($to, $name, $subject = '', $body = '', $attachment = null){
+
+         $config = C('3S_EMAIL');
+         vendor('PHPMailer.class#phpmailer'); //从PHPMailer目录导class.phpmailer.php类文件
+         $mail             = new PHPMailer(); //PHPMailer对象
+         $mail->CharSet    = 'UTF-8'; //设定邮件编码，默认ISO-8859-1，如果发中文此项必须设置，否则乱码
+         $mail->IsSMTP();  // 设定使用SMTP服务
+         $mail->SMTPDebug  = true;                     // 关闭SMTP调试功能
+                                                    // 1 = errors and messages
+                                                    // 2 = messages only
+         $mail->SMTPAuth   = true;                  // 启用 SMTP 验证功能
+         //$mail->SMTPSecure = 'ssl';                 // 使用安全协议
+         $mail->Host       = $config['SMTP_HOST'];  // SMTP 服务器
+         //$mail->Port       = $config['SMTP_PORT'];  // SMTP服务器的端口号
+         $mail->Username   = $config['SMTP_USER'];  // SMTP服务器用户名
+         $mail->Password   = $config['SMTP_PASS'];  // SMTP服务器密码
+         $mail->SetFrom($config['FROM_EMAIL'], $config['FROM_NAME']);
+         $replyEmail       = $config['REPLY_EMAIL']?$config['REPLY_EMAIL']:$config['FROM_EMAIL'];
+         $replyName        = $config['REPLY_NAME']?$config['REPLY_NAME']:$config['FROM_NAME'];
+         $mail->AddReplyTo($replyEmail, $replyName);
+         $mail->Subject    = $subject;
+         $mail->MsgHTML($body);
+         $mail->AddAddress($to, $name);
+         if(is_array($attachment)){ // 添加附件
+             foreach ($attachment as $file){
+                 is_file($file) && $mail->AddAttachment($file);
+             }
+         }
+         return $mail->Send() ? true : $mail->ErrorInfo;
+    }
+
+    private function inventoryWarning(){
+        $data = M(C('DB_USSTORAGE'))->select();
+        $warning = null;
+        $indexOfWarning = 0;
+        $product = M(C('DB_PRODUCT'));
+        $user = M(C('DB_USER'));
+        foreach ($data as $key => $value) {
+            if($value[C('DB_USSTORAGE_AINVENTORY')]<=1){
+                $manager = $product->where(array(C('DB_PRODUCT_SKU')=>$value[C('DB_USSTORAGE_SKU')]))->getField(C('DB_PRODUCT_MANAGER'));
+                $warning[$indexOfWarning][C('DB_PRODUCT_MANAGER')] = $manager;
+                $warning[$indexOfWarning][C('DB_PRODUCT_SKU')] = $value[C('DB_USSTORAGE_SKU')];
+                $indexOfWarning = $indexOfWarning+1;
+            }
+        }
+
+        /*if($warning != null){
+            foreach ($warning as $key => $w) {
+                $email = $user->where(array(C('DB_3S_USER_USERNAME')=>$key))->getField(C('DB_3s_USER_EMAIL'));
+                if($email != null){
+                    $body = $w."美国自建仓库存数量不足，请求改在线刊登的ebay,amazon数量";
+                    $this->sendInventoryMail($email,$manager,'缺货提醒',$body);
+                }
+            }
+            
+        }*/
+        return $warning;
+    }
+
+    public function checkAinventory(){
+      $usstorage=$this->inventoryWarning();
+      $this->assign('usstorage',$usstorage);
+      $this->display();
+    }
 }
 
 ?>
