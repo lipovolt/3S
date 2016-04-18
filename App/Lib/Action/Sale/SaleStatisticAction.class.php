52adbb7ed7d9bb16ed1a2997321a9ebaf9bfd1e6
lipovolt <498307481@qq.com>
2016-04-18 22:08:00 +0800

    1

---
+++ App/Lib/Action/Sale/SaleStatisticAction.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class SaleStatisticAction extends CommonAction{
+	public function index(){
+		
+	}
+}
+
+?>
--- App/Lib/Action/Ussw/OutboundAction.class.php
+++ App/Lib/Action/Ussw/OutboundAction.class.php
@@ -22,16 +22,7 @@ class OutboundAction extends CommonAction{
 	}
 
 	public function itemOutbound(){
-        $result = $this->sendInventoryMail('498307481@qq.com', 'zhangyu', $subject = 'inventory warnning', $body = 'sku 1000 = 0', $attachment = null);
-        dump($result);
-        die;
-        if($result){
-            $this->success('邮件发送成功');
-        }else{
-            $this->error($result,'',10);
-        }
-
-        /*if(IS_POST){
+        if(IS_POST){
             if(I('post.sku','','htmlspecialchars')!=''){
                 if(I('post.position','','htmlspecialchars') == ''){
                     $where[C('DB_USSTORAGE_SKU')] = I('post.sku','','htmlspecialchars');
@@ -63,7 +54,7 @@ class OutboundAction extends CommonAction{
                 }
             }
             
-        }*/
+        }
     }
 
     public function itemBatchOutbound(){
@@ -326,7 +317,6 @@ class OutboundAction extends CommonAction{
                             }
 
                         }
-
                     }
                     $usswOutbound->commit();
                     $usswOutboundItem->commit();
@@ -427,46 +417,6 @@ class OutboundAction extends CommonAction{
         else{
             return true;
         }
-    }
-
-    /**
-      * 系统邮件发送函数
-      * @param string $to    接收邮件者邮箱
-      * @param string $name  接收邮件者名称
-      * @param string $subject 邮件主题 
-      * @param string $body    邮件内容
-      * @param string $attachment 附件列表
-      * @return boolean 
-      */
-    private function sendInventoryMail($to, $name, $subject = '', $body = '', $attachment = null){
-
-         $config = C('3S_EMAIL');
-         vendor('PHPMailer.class#phpmailer'); //从PHPMailer目录导class.phpmailer.php类文件
-         $mail             = new PHPMailer(); //PHPMailer对象
-         $mail->CharSet    = 'UTF-8'; //设定邮件编码，默认ISO-8859-1，如果发中文此项必须设置，否则乱码
-         $mail->IsSMTP();  // 设定使用SMTP服务
-         $mail->SMTPDebug  = true;                     // 关闭SMTP调试功能
-                                                    // 1 = errors and messages
-                                                    // 2 = messages only
-         $mail->SMTPAuth   = true;                  // 启用 SMTP 验证功能
-         //$mail->SMTPSecure = 'ssl';                 // 使用安全协议
-         $mail->Host       = $config['SMTP_HOST'];  // SMTP 服务器
-         $mail->Port       = $config['SMTP_PORT'];  // SMTP服务器的端口号
-         $mail->Username   = $config['SMTP_USER'];  // SMTP服务器用户名
-         $mail->Password   = $config['SMTP_PASS'];  // SMTP服务器密码
-         $mail->SetFrom($config['FROM_EMAIL'], $config['FROM_NAME']);
-         $replyEmail       = $config['REPLY_EMAIL']?$config['REPLY_EMAIL']:$config['FROM_EMAIL'];
-         $replyName        = $config['REPLY_NAME']?$config['REPLY_NAME']:$config['FROM_NAME'];
-         $mail->AddReplyTo($replyEmail, $replyName);
-         $mail->Subject    = $subject;
-         $mail->MsgHTML($body);
-         $mail->AddAddress($to, $name);
-         if(is_array($attachment)){ // 添加附件
-             foreach ($attachment as $file){
-                 is_file($file) && $mail->AddAttachment($file);
-             }
-         }
-         return $mail->Send() ? true : $mail->ErrorInfo;
     }
 }
 
