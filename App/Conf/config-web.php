a1b3e642f2f0fe413289e6a6b436b2b74dcd1a51
lipovolt <498307481@qq.com>
2016-09-19 09:39:30 +0800

    1

--- App/Conf/config-web.php
+++ App/Conf/config-web.php
@@ -35,7 +35,7 @@ return array(
 	'USER_AUTH_ON' => true,
 	'USER_AUTH_TYPE' =>1,
 	'USER_AUTH_KEY' => 'uid',
-	'NOT_AUTH_MODULE' => 'Index,Product,Purchase,Supplier,GgsUsswSale,Metadata,WinitDeSale,WinitUsSale,Restock,Storage,Inbound,Outbound,Storage',
+	'NOT_AUTH_MODULE' => 'Product,Index,Purchase,Restock,Supplier,GgsUsswSale,SzSale,WinitDeSale,WinitUsSale,Storage,Inbound,Outbound,Postage',
 	'NOT_AUTH_ACTION' => 'login,logout',
 	'RBAC_ROLE_TABLE' => '3s_role',
 	'RBAC_USER_TABLE' => '3s_role_user',
