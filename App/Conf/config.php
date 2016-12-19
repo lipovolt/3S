dad07995400ca1c578fe654e06c0742a06869790
lipovolt <498307481@qq.com>
2016-12-19 22:21:27 +0800

    1

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -7,7 +7,7 @@ return array(
 	'APP_DEBUG' => True,
 
 	//开启应用分组
-	'APP_GROUP_LIST' => 'Admin,Index,Product,Purchase,Sale,Storage,Ussw,Szsw,Todo',
+	'APP_GROUP_LIST' => 'Admin,Index,Product,Purchase,Sale,Ussw,Szsw,Todo',
 	'DEFAULT_GROUP' => 'Index',
 
 	//定义数据库连接参数
